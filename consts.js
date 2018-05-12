804a21b529ae6b126b00f4ce23bf45228e362e66
techgaun <coolsamar207@gmail.com>
2018-05-12 01:00:48 -0500

    initial set of handlers

---
+++ consts.js
@@ -0,0 +1,9 @@
+exports.messages = {
+  launch: 'Welcome to country quiz',
+  stop: 'Bye',
+  help: [
+    'Start with selecting one of the categories from continent, capital and currency or all',
+    'Answer the questions from Country Quiz',
+    'Complete at any time by saying finish the game'
+  ]
+}
--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 'use strict'
 
 const Alexa = require('alexa-sdk')
+const consts = require('./consts')
+
+const handlers = {
+  'LaunchRequest': () => {
+    this.response.speak(consts.messages['launch'])
+    this.emit(':responseReady')
+  },
+  'AMAZON.StopIntent': () => {
+    this.response.speak(consts.messages['stop'])
+    this.emit(':responseReady')
+  },
+  'AMAZON.HelpIntent': () => {
+    help_lines = consts.messages['help']
+    help_lines.forEach(help => {
+      this.response.speak(help)
+    })
+    this.emit(':responseReady')
+  },
+  'AMAZON.CancelIntent': () => {
+    this.response.speak(consts.messages['stop'])
+    this.emit(':responseReady')
+  },
+  'Unhandled': () => {
+    this.emit('AMAZON.HelpIntent')
+  }
+}
+
+exports.handler = (event, context, _callback) => {
+  const alexa = Alexa.handler(event, context)
+  alexa.registerHandlers(handlers)
+  alexa.execute()
+}
